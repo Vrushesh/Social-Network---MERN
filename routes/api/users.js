8b36d64cbbf9e391f9ab4a4bcc248fd63ec83e73
Vrushesh <patelv12@mcmaster.caext>
2018-11-11 07:17:46 -0500

    POST call created for new years to sign up to the entwrok

--- routes/api/users.js
+++ routes/api/users.js
@@ -26,8 +26,6 @@ router.post("/register", (req, res) => {
       });
 
       const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
         avatar: avatar,
         password: req.body.password
       });
