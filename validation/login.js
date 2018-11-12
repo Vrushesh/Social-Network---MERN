638daf06c9b61f5f1e99711b1bb0d779a5822dbc
Vrushesh <patelv12@mcmaster.caext>
2018-11-11 23:46:09 -0500

    User login and registration input validation on backend complete

---
+++ validation/login.js
@@ -0,0 +1,28 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  // Validator isEmpty method only checks strings - use our isEmpty to check for null val, etc.
+
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  return {
+    errors: errors,
+    isValid: isEmpty(errors)
+  };
+};
