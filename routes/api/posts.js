13b79987c70ab1aecb290e0ea1fdd311c736a437
Vrushesh <vrusheshp@gmail.com>
2018-11-18 15:28:00 -0500

    Get all and indivdual post routes added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -14,31 +14,28 @@ const validatePostInput = require("../../validation/post");
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
 
-// @route   POST api/posts
-// @desc    Create post
-// @access  Private
-router.post(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    const { errors, isValid } = validatePostInput(req.body);
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that ID" })
+    );
+});
 
-    // Check Validation
-    if (!isValid) {
-      // Return any errors with 400 status
-      return res.status(400).json(errors);
-    }
-
-    const newPost = new Post({
-      text: req.body.text,
-      name: req.body.name,
-      avatar: req.body.avatar,
-      user: req.user.id
-    });
-
-    newPost.save().then(post => res.json(post));
-  }
-);
+// @route   GET api/posts/:id
+// @desc    Get posts by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that ID" })
+    );
+});
 
 // @route   POST api/posts
 // @desc    Create post
